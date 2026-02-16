b16f1eb3186823e0a4f9e61c3aee6c2a835e9614
JAPHES <japhesmurithi@gmail.com>
2026-02-16 16:43:54 +0300

    first commit

---
+++ techie/urls.py
@@ -0,0 +1,12 @@
+﻿from django.urls import path
+
+from . import views
+
+app_name = 'techie'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('portfolio-details/', views.portfolio_details, name='portfolio_details'),
+    path('service-details/', views.service_details, name='service_details'),
+    path('starter-page/', views.starter_page, name='starter_page'),
+]
---
+++ techie/views.py
@@ -0,0 +1,17 @@
+﻿from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'techie/index.html')
+
+
+def portfolio_details(request):
+    return render(request, 'techie/portfolio-details.html')
+
+
+def service_details(request):
+    return render(request, 'techie/service-details.html')
+
+
+def starter_page(request):
+    return render(request, 'techie/starter-page.html')
